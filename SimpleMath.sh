4a80a1b1c0b826b6a6f0c0ebf41b2c2d5c2dec2e
Ikwuka A. Ch. Okoye <maziarinze@protonmail.com>
2022-06-02 18:29:59 -0400

    Start up with an expressive script

---
+++ SimpleMath.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# This script evaluates simplications using /
+# the PEMDAS (parenthesis, exponenetiation, / 
+# multiplication, division, addition, and / 
+# subtraction) solution
+
+# STILL TO BE COMPLETEd
+ 
