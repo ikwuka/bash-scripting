a7b649e721738af8db35a75ac59b2a8beaa943d0
Ikwuka A. Ch. Okoye <maziarinze@protonmail.com>
2022-06-02 19:00:44 -0400

    Alter the MSSQLscanner script

--- MSSQLscanner.sh
+++ MSSQLscanner.sh
@@ -2,7 +2,17 @@
 
 # This script scans for the Microsoft SQL Server open port
 
-nmap -sT 193.168.181.1 -p 1433 > /dev/null -oG MSSQLscan
+
+echo "Enter a starting IP address: "
+read startingIP
+
+echo "Enter your ending IP address: "
+read lastIP
+
+echo "Enter the port you wish to scan: "
+read port
+
+nmap -sT $startingIP-$lastIP -p $port > /dev/null -oG MSSQLscan
 
 cat $MSSQLscan | grep open > MSSQLscan2
 
