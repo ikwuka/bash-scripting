02ed726be0f834a0928a316da0eb499c1d93d21c
Ikwuka A. Ch. Okoye <maziarinze@protonmail.com>
2022-06-03 07:25:12 -0400

    Create first evaluation script

--- eval.sh
+++ eval.sh
@@ -1,2 +1,12 @@
+command='ls -lrt'
 
+echo 'This command will find the list of files in a directory'
+
+echo "Before that, the '$command' variable will be executed with eval"
+
+$command
+
+echo "A simple evaluation will be performed now"
+
+eval $command
  
