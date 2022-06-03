1dc11a41900069cdd73ee305fbfd4d5844752e4a
Ikwuka A. Ch. Okoye <maziarinze@protonmail.com>
2022-06-03 18:33:00 -0400

    Copy files to be archived

---
+++ new-scripts/tarballed/shellThree.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+greetingOne="thank you!"
+
+greetingTwo="Thank you"
+
+blessing="remain blessed!"
+
+echo "Thank you for joining me."
+
+echo "What else can I say than '$greetingOne'"
+
+echo "From me from here ...:" $greetingTwo "and" $blessing
