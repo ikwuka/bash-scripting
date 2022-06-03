48e28481fbd3a500e624fe4ad538fb7f2306ec0b
Ikwuka A. Ch. Okoye <maziarinze@protonmail.com>
2022-06-03 07:19:49 -0400

    Rename SimpleMath script

--- SimpleMath.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# This script evaluates simplications using /
-# the PEMDAS (parenthesis, exponenetiation, / 
-# multiplication, division, addition, and / 
-# subtraction) solution
-
-# STILL TO BE COMPLETEd
- 
